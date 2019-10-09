514ff1a543211f670d0d33224a3531f5c811782c
Phu.Nguyen <Phu.Nguyen@altsrc.net>
2019-10-09 11:34:44 +0700

    Add validation for login request

--- FlirtingApp.Api/RequestModels/LoginRequest.cs
+++ FlirtingApp.Api/RequestModels/LoginRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace FlirtingApp.Api.RequestModels
 {
 	public class LoginRequest
 	{
+		[Required]
 		public string UserName { get; set; }
+		[StringLength(maximumLength: 8, MinimumLength = 4, ErrorMessage = "Password has to be bet 4 - 8 characters")]
 		public string Password { get; set; }
 	}
 }
