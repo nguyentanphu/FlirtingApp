5d8639d3faf50bb78fb30d90fe1fe57b16532cad
PhuNguyen <nguyentanphu@hotmail.com>
2019-11-18 21:20:52 +0700

    Refactor infrastructure DI

--- FlirtingApp.Infrastructure/DependencyInjection.cs
+++ FlirtingApp.Infrastructure/DependencyInjection.cs
@@ -5,6 +5,7 @@ using FlirtingApp.Application.Common.Interfaces;
 using FlirtingApp.Infrastructure.ConfigOptions;
 using FlirtingApp.Infrastructure.Identity;
 using FlirtingApp.Infrastructure.Identity.Models;
+using FlirtingApp.Infrastructure.Registras;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -23,50 +24,7 @@ namespace FlirtingApp.Infrastructure
 			services.AddScoped<IAppIdentityDbContext>(provider => provider.GetService<AppIdentityDbContext>());
 			services.AddScoped<IAppUserManager, AppAppUserManager>();
 
-			var authSettings = configuration.GetSection(nameof(AuthOptions));
-			var jwtOptions = configuration.GetSection(nameof(JwtOptions));
-			var signingKey =
-				new SymmetricSecurityKey(Encoding.ASCII.GetBytes(authSettings[nameof(AuthOptions.JwtSecret)]));
-			services.Configure<JwtOptions>(options =>
-			{
-				options.Issuer = jwtOptions[nameof(JwtOptions.Issuer)];
-				options.Audience = jwtOptions[nameof(JwtOptions.Audience)];
-				options.SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha512Signature);
-			});
-			services.Configure<AuthOptions>(authSettings);
-			services.Configure<CloudinaryCredential>(configuration.GetSection(nameof(CloudinaryCredential)));
-
-			services.AddIdentityCore<AppUser>(o =>
-			{
-				// configure identity options
-				o.Password.RequireDigit = false;
-				o.Password.RequireLowercase = false;
-				o.Password.RequireUppercase = false;
-				o.Password.RequireNonAlphanumeric = false;
-				o.Password.RequiredLength = 6;
-			}).AddEntityFrameworkStores<AppIdentityDbContext>();
-
-			services.AddAuthentication(options =>
-			{
-				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-			}).AddJwtBearer(options =>
-			{
-				var tokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateIssuer = true,
-					ValidIssuer = jwtOptions[nameof(JwtOptions.Issuer)],
-					ValidateAudience = true,
-					ValidAudience = jwtOptions[nameof(JwtOptions.Audience)],
-					ValidateIssuerSigningKey = true,
-					IssuerSigningKey = signingKey,
-					ValidateLifetime = true,
-					ClockSkew = TimeSpan.Zero
-				};
-
-				options.ClaimsIssuer = jwtOptions[nameof(JwtOptions.Issuer)];
-				options.TokenValidationParameters = tokenValidationParameters;
-			});
+			services.AddCustomJwtAuthentication(configuration);
 			return services;
 		}
 	}
---
+++ FlirtingApp.Infrastructure/Identity/JwtFactory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using FlirtingApp.Infrastructure.ConfigOptions;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace FlirtingApp.Infrastructure.Identity
+{
+	public interface IJwtFactory
+	{
+		string GenerateEncodedTokens(Guid id, string userName);
+		ClaimsPrincipal GetClaimPrinciple(string accessToken, SecurityKey signingKey);
+	}
+
+	public class JwtFactory : IJwtFactory
+	{
+		private readonly JwtOptions _jwtOptions;
+		private readonly JwtSecurityTokenHandler _jwtHandler;
+
+		public JwtFactory(IOptions<JwtOptions> jwtOptions, JwtSecurityTokenHandler jwtHandler)
+		{
+			_jwtHandler = jwtHandler;
+			_jwtOptions = jwtOptions.Value;
+		}
+
+		public string GenerateEncodedTokens(Guid id, string userName)
+		{
+			var claims = new[]
+			{
+				new Claim("id", id.ToString()),
+				new Claim("rol", "api_access"),
+				new Claim(JwtRegisteredClaimNames.Sub, userName),
+				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+			};
+
+			var jwt = new JwtSecurityToken(
+				new JwtHeader(_jwtOptions.SigningCredentials), 
+				new JwtPayload(_jwtOptions.Issuer, _jwtOptions.Audience, claims, _jwtOptions.NotBefore, _jwtOptions.Expiration, _jwtOptions.IssuedAt)
+			);
+
+			return _jwtHandler.WriteToken(jwt);
+		}
+
+		public ClaimsPrincipal GetClaimPrinciple(string accessToken, SecurityKey signingKey)
+		{
+			return _jwtHandler.ValidateToken(accessToken, new TokenValidationParameters
+			{
+				ValidateIssuer = false,
+				ValidateAudience = false,
+				ValidateIssuerSigningKey = true,
+				IssuerSigningKey = signingKey,
+				ValidateLifetime = false,
+			}, out _);
+		}
+
+	}
+}
