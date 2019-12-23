bf5052c4a14ae635efab95e15decd2049a51690e
Phu.Nguyen <Phu.Nguyen@altsrc.net>
2019-12-23 18:03:08 +0700

    Update WebApi presenters tests

---
+++ FlirtingApp.WebApi.Tests/ApiPresenters/CreateUserPresenterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlirtingApp.Application.Users.Commands.CreateUser;
+using FlirtingApp.WebApi.ApiPresenters;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace FlirtingApp.WebApi.Tests.ApiPresenters
+{
+	public class CreateUserPresenterTests
+	{
+		private readonly CreateUserPresenter _sut = new CreateUserPresenter();
+
+		[Fact]
+		public void Handle_Fail_ShouldBeBadRequestObjectResult()
+		{
+			_sut.Handle(new CreateUserCommandResponse
+			{
+				Success = false,
+				ErrorMessage = "Fail with unknown reason"
+			});
+
+			_sut.Result.Should().BeOfType<BadRequestObjectResult>();
+		}
+
+		[Fact]
+		public void Handle_Success_ShouldBeBadRequestObjectResult()
+		{
+			_sut.Handle(new CreateUserCommandResponse
+			{
+				Success = true
+			});
+
+			_sut.Result.Should().BeOfType<CreatedAtRouteResult>();
+		}
+	}
+}
