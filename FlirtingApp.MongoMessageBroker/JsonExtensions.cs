fb11e0f9f18c26f2aa51afa89ba8f53324e16632
huy.tran <tqhuy1811@gmail.com>
2020-01-30 20:33:37 +0700

    second attempt at adding rabbit mq

---
+++ FlirtingApp.MongoMessageBroker/JsonExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+
+namespace FlirtingApp.Infrastructure.MongoMessageBroker
+{
+	public class ContractResolverWithPrivates : Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver
+	{
+		protected override Newtonsoft.Json.Serialization.JsonProperty CreateProperty(
+			MemberInfo member, MemberSerialization memberSerialization)
+		{
+			var prop = base.CreateProperty(member, memberSerialization);
+
+			if (!prop.Writable)
+			{
+				var property = member as PropertyInfo;
+				if (property != null)
+				{
+					var hasPrivateSetter = property.GetSetMethod(true) != null;
+					prop.Writable = hasPrivateSetter;
+				}
+			}
+
+			return prop;
+		}
+	}
+
+	public class CustomJsonSerializerSettings
+	{
+		public static JsonSerializerSettings GetSerializerSettings()
+		{
+			var settings = new JsonSerializerSettings();
+			settings.ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor;
+			settings.ContractResolver = new ContractResolverWithPrivates();
+
+			return settings;
+		}
+	}
+}
